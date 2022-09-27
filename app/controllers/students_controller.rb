e6646a1b21c4c318e75968968ead0671eaafe650
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-09-27 09:40:37 +0300

    created a dynamic route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,8 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
-
+  def by_student_id
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
